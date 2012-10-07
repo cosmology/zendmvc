e69fce3ac9f3e246e892e5b42af7859109ee77a1
Ivan Prokic <ivanprokic@yahoo.com>
2012-10-06 21:59:14 -0700

    Forgetten files for langage translation

---
+++ application/modules/admin/languages/en/routes.php
@@ -0,0 +1,16 @@
+<?php
+// English
+return array(
+		'home'		=> 'home',
+		'about'		=> 'about',
+		'user'		=> 'user',
+		'search'	=> 'search',
+		'contact'	=> 'contact',
+		'admin'		=> 'admin',
+		'home_header_title' => 'Zend MVC demo app',
+		'home_body_h2_header' => 'Welcome to Ivan\'s Zend presentation',
+		'contact_body_h2_header' => 'Start Your Conversation Now'
+
+		// ...
+);
+?>
